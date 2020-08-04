a86bbe8df7a857944db8dfbdd0dadbae20a60b12
Jerrett Longworth <jerrett7@gmail.com>
2020-08-04 17:36:19 -0400

    SearchActivity: Simplify ArrayList data management
    Instead of making a new ArrayList when we parse the JSON data, let's make the ArrayList at the beginning, add the items to it, then tell the adapter to refresh itself. I'm also clearing out some now-unused method parameters for the sake of simplicity. For example, if we don't need to pass the Context around, just don't.
    
    This will change in the future though, as I eventually plan to move the networking and data parsing functionality to a separate class.

--- app/src/main/java/com/malseriesguideextension/SearchActivity.java
+++ app/src/main/java/com/malseriesguideextension/SearchActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,7 +29,7 @@ public class SearchActivity extends AppCompatActivity {
     private LinearLayout progressOverlay;
     private LinearLayout errorOverlay;
     private RequestQueue queue;
-    private RecyclerView recyclerView;
+    private AnimeAdapter animeAdapter;
 
     private ArrayList<AnimeSearchResult> results;
 
@@ -53,11 +52,15 @@ public class SearchActivity extends AppCompatActivity {
         errorOverlay = findViewById(R.id.error_overlay);
 
         // Set up RecyclerView (list of anime results).
-        recyclerView = findViewById(R.id.recycler_view);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // Create results array and attach the data to an adapter, then the adapter to the RecyclerView.
         results = new ArrayList<>();
+        animeAdapter = new AnimeAdapter(results, this);
+        recyclerView.setAdapter(animeAdapter);
+
 
         // Grab the information to be searched from the intent.
         Intent intent = getIntent();
@@ -68,7 +71,7 @@ public class SearchActivity extends AppCompatActivity {
             finish();
         }
 
-        makeApiRequest(text, this);
+        makeApiRequest(text);
     }
 
 
@@ -85,19 +88,28 @@ public class SearchActivity extends AppCompatActivity {
     }
 
 
-    private void makeApiRequest(String query, final Context context) {
+    private void makeApiRequest(String query) {
         query = "https://api.jikan.moe/v3/search/anime?q=" + query + "&page=1?limit=10";
 
         // Set up the request to the internet
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, query, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                results = parseJson(response);
-                displayResults(context);
+                // We got a list of results, let's do something with them.
+                try {
+                    // Parse the JSON and display it.
+                    parseJson(results, response);
+                    displayResults();
+                }
+                catch (Exception exception) {
+                    // If something goes wrong during parsing, assume an error.
+                    displayError();
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                // We weren't able to get the results. Tell the user as such.
                 displayError();
             }
         });
@@ -108,48 +120,41 @@ public class SearchActivity extends AppCompatActivity {
 
 
     /**
-     * Convert the JSON received from MAL into a usable ArrayList of AnimeSearchResult objects.
+     * Parse JSON results into AnimeSearchResult objects, then add them to a provided ArrayList.
      *
+     * @param list The ArrayList to add items to.
      * @param json The JSON data to be parsed.
-     * @return A new ArrayList of AnimeSearchResult objects based on the contents of the JSON data.
      */
-    private ArrayList<AnimeSearchResult> parseJson(JSONObject json) {
-        JSONArray inList;
-        ArrayList<AnimeSearchResult> outList = new ArrayList<>();
+    private void parseJson(ArrayList<AnimeSearchResult> list, JSONObject json) throws Exception {
+        JSONArray inList = json.getJSONArray("results");
 
-        try {
-            inList = json.getJSONArray("results");
-
-            for (int i = 0; i < inList.length(); i++) {
-                JSONObject currentItem = inList.getJSONObject(i);
-                AnimeSearchResult newItem = new AnimeSearchResult(
-                        currentItem.getString("title"),
-                        currentItem.getString("url"));
-                outList.add(newItem);
-            }
+        for (int i = 0; i < inList.length(); i++) {
+            JSONObject currentItem = inList.getJSONObject(i);
+            AnimeSearchResult newItem = new AnimeSearchResult(
+                    currentItem.getString("title"),
+                    currentItem.getString("url"));
+            list.add(newItem);
         }
-        catch (Exception exception) {
-            outList = null;
-        }
-
-        return outList;
     }
 
+    /**
+     * Remove anything on the screen that indicates we were loading something.
+     */
     private void finishLoading() {
         progressOverlay.setVisibility(View.GONE);
     }
 
-    private void displayResults(Context context) {
-        // Set the contents of the RecyclerView to the search results.
+    /**
+     * Update the RecyclerView's adapter with the newly-added search results.
+     */
+    private void displayResults() {
         finishLoading();
-        if (results != null) {
-            recyclerView.setAdapter(new AnimeAdapter(results, context));
-        }
-        else {
-            displayError();
-        }
+        animeAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * Something wrong happened, so display a message saying so.
+     */
     private void displayError() {
         finishLoading();
         errorOverlay.setVisibility(View.VISIBLE);
